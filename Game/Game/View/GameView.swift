661cdeadd71f818302f00a6cdd94640b21e50c78
Kevin <kevinw5@live.com>
2018-04-19 12:36:19 -0600

    Finish game view

--- Game/Game/View/GameView.swift
+++ Game/Game/View/GameView.swift
@@ -24,63 +24,54 @@ class GameView: UIView {
     
     let topLeftButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .red
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     let topCenterButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .blue
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     let topRightButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .yellow
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     let centerLeftButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .green
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     let centerButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .purple
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     let centerRightButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .orange
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     let bottomLeftButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .gray
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     let bottomCenterButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .darkGray
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     let bottomRightButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = .lightGray
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -108,7 +99,7 @@ class GameView: UIView {
     
     var gameController: GameViewController? {
         didSet {
-            topLeftButton.addTarget(gameController, action: #selector(gameController?.moveMade), for: .touchUpInside)
+            
         }
     }
     
@@ -128,6 +119,14 @@ class GameView: UIView {
         addSubview(instructionLabel)
         addSubview(boardImageView)
         boardImageView.addSubview(topLeftButton)
+        boardImageView.addSubview(topCenterButton)
+        boardImageView.addSubview(topRightButton)
+        boardImageView.addSubview(centerLeftButton)
+        boardImageView.addSubview(centerButton)
+        boardImageView.addSubview(centerRightButton)
+        boardImageView.addSubview(bottomLeftButton)
+        boardImageView.addSubview(bottomCenterButton)
+        boardImageView.addSubview(bottomRightButton)
         
         instructionLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         instructionLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 40).isActive = true
@@ -139,8 +138,47 @@ class GameView: UIView {
         
         topLeftButton.leftAnchor.constraint(equalTo: boardImageView.leftAnchor).isActive = true
         topLeftButton.topAnchor.constraint(equalTo: boardImageView.topAnchor).isActive = true
-        topLeftButton.widthAnchor.constraint(equalToConstant: 97).isActive = true
-        topLeftButton.heightAnchor.constraint(equalToConstant: 152.5).isActive = true
+        topLeftButton.widthAnchor.constraint(equalTo: boardImageView.widthAnchor, multiplier: 0.27).isActive = true
+        topLeftButton.heightAnchor.constraint(equalTo: boardImageView.heightAnchor, multiplier: 0.31).isActive = true
         
+        topCenterButton.centerXAnchor.constraint(equalTo: boardImageView.centerXAnchor).isActive = true
+        topCenterButton.topAnchor.constraint(equalTo: boardImageView.topAnchor).isActive = true
+        topCenterButton.widthAnchor.constraint(equalTo: boardImageView.widthAnchor, multiplier: 0.38).isActive = true
+        topCenterButton.heightAnchor.constraint(equalTo: boardImageView.heightAnchor, multiplier: 0.32).isActive = true
+        
+        topRightButton.rightAnchor.constraint(equalTo: boardImageView.rightAnchor).isActive = true
+        topRightButton.topAnchor.constraint(equalTo: boardImageView.topAnchor).isActive = true
+        topRightButton.widthAnchor.constraint(equalTo: boardImageView.widthAnchor, multiplier: 0.26).isActive = true
+        topRightButton.heightAnchor.constraint(equalTo: boardImageView.heightAnchor, multiplier: 0.31).isActive = true
+        
+        centerLeftButton.centerYAnchor.constraint(equalTo: boardImageView.centerYAnchor).isActive = true
+        centerLeftButton.leftAnchor.constraint(equalTo: boardImageView.leftAnchor).isActive = true
+        centerLeftButton.widthAnchor.constraint(equalTo: boardImageView.widthAnchor, multiplier: 0.27).isActive = true
+        centerLeftButton.heightAnchor.constraint(equalTo: boardImageView.heightAnchor, multiplier: 0.31).isActive = true
+        
+        centerButton.centerXAnchor.constraint(equalTo: boardImageView.centerXAnchor).isActive = true
+        centerButton.centerYAnchor.constraint(equalTo: boardImageView.centerYAnchor).isActive = true
+        centerButton.widthAnchor.constraint(equalTo: boardImageView.widthAnchor, multiplier: 0.38).isActive = true
+        centerButton.heightAnchor.constraint(equalTo: boardImageView.heightAnchor, multiplier: 0.32).isActive = true
+        
+        centerRightButton.rightAnchor.constraint(equalTo: boardImageView.rightAnchor).isActive = true
+        centerRightButton.centerYAnchor.constraint(equalTo: boardImageView.centerYAnchor).isActive = true
+        centerRightButton.widthAnchor.constraint(equalTo: boardImageView.widthAnchor, multiplier: 0.26).isActive = true
+        centerRightButton.heightAnchor.constraint(equalTo: boardImageView.heightAnchor, multiplier: 0.31).isActive = true
+        
+        bottomLeftButton.leftAnchor.constraint(equalTo: boardImageView.leftAnchor).isActive = true
+        bottomLeftButton.bottomAnchor.constraint(equalTo: boardImageView.bottomAnchor).isActive = true
+        bottomLeftButton.widthAnchor.constraint(equalTo: boardImageView.widthAnchor, multiplier: 0.27).isActive = true
+        bottomLeftButton.heightAnchor.constraint(equalTo: boardImageView.heightAnchor, multiplier: 0.31).isActive = true
+        
+        bottomCenterButton.centerXAnchor.constraint(equalTo: boardImageView.centerXAnchor).isActive = true
+        bottomCenterButton.bottomAnchor.constraint(equalTo: boardImageView.bottomAnchor).isActive = true
+        bottomCenterButton.widthAnchor.constraint(equalTo: boardImageView.widthAnchor, multiplier: 0.38).isActive = true
+        bottomCenterButton.heightAnchor.constraint(equalTo: boardImageView.heightAnchor, multiplier: 0.32).isActive = true
+        
+        bottomRightButton.rightAnchor.constraint(equalTo: boardImageView.rightAnchor).isActive = true
+        bottomRightButton.bottomAnchor.constraint(equalTo: boardImageView.bottomAnchor).isActive = true
+        bottomRightButton.widthAnchor.constraint(equalTo: boardImageView.widthAnchor, multiplier: 0.26).isActive = true
+        bottomRightButton.heightAnchor.constraint(equalTo: boardImageView.heightAnchor, multiplier: 0.31).isActive = true
     }
 }
